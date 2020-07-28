d4c93c7a57cec958d2d5069a31ac56f1669cbf50
Takudzwa Chidarikire <briansaved@gmail.com>
2020-07-28 17:08:16 +1000

    All books populated after component did mount and stored in state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import * as BooksAPI from "./BooksAPI";
 import { Route, Link } from "react-router-dom";
 import "./App.css";
 import Search from "./Search";
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
+    books: [], //will be populated when component mounts
   };
+
+  //Called in to make API call as this is the best place
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      //state now set and populated directly from API
+      this.setState({
+        books,
+      });
+      // console.log(this.state.books);
+    });
+  }
 
   render() {
     return (
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    query: "",
+  };
+
   render() {
     return <div className="root-search"></div>;
   }
